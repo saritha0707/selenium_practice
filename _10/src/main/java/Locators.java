8ff1027d5d9ec2685660c7b873032aca7d21442e
Schilak <saritha.chilakalapudi@macys.com>
2021-01-25 20:29:49 +0530

    Locators

---
+++ _10/src/main/java/Locators.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class Locators {
+
+    public static void main(String[] args)
+    {
+        WebDriver driver;
+        ChromeOptions options = new ChromeOptions(); options.addArguments("disable-infobars");
+        System.setProperty("webdriver.chrome.driver","D:\\Nisum_2021\\selenium_drivers\\chromedriver.exe");
+        driver = new ChromeDriver(options);
+        try
+        {
+            driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+            driver.get("https://www.techgig.com/");
+            driver.manage().window().maximize();
+
+            //LinkText
+            driver.findElement(By.linkText("Login")).click();
+            Thread.sleep(1000);
+            //Click on SignUp using partialLinkText
+            driver.findElement(By.partialLinkText("Sign")).click();
+
+            //select professional radio button using id
+            driver.findElement(By.id("professional")).click();
+
+            //enter first_name using name locator
+            driver.findElement(By.name("first_name")).sendKeys("ABC");
+
+            //enter email using css selector
+            driver.findElement(By.cssSelector("#register_email")).sendKeys("abcxyz@mailinator.com");
+
+
+            //select agreement using classname
+            driver.findElement(By.className("agreement")).click();
+
+            Thread.sleep(3000);
+            //   driver.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            driver.close();
+        }
+    }
+}
